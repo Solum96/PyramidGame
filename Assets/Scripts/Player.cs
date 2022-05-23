9d288c3001fe9a280c8b7d1712f069436e6ef5fc
Nils Lundell <nils.lundell@mardigras.se>
2022-05-23 12:35:34 +0200

    Made player into pyramid. added rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,8 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float rotationSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +24,6 @@ public class Player : MonoBehaviour
     private void Move(float input)
     {
         transform.position += new Vector3(input, 0, 0) * moveSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.up * (rotationSpeed * -input * 100) * Time.deltaTime);
     }
 }

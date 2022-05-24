9bb33c7f662a5d8be22cbf7b04bfd507b0995174
Nils Lundell <nils.lundell@mardigras.se>
2022-05-24 07:53:49 +0200

    Created an objectPooler

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Meteor : MonoBehaviour
+public class Meteor : MonoBehaviour, IPooledObject
 {
     [SerializeField]
     private float fallSpeed;
-    // Start is called before the first frame update
-    void Start()
+
+    public void OnObjectSpawn()
     {
         
     }
+
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton (sort of)
+
+    public static ObjectPooler Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    #endregion
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach (var pool in pools)
+        {
+            var objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.size; i++)
+            {
+                var gameObject = Instantiate(pool.prefab);
+                gameObject.SetActive(false);
+                objectPool.Enqueue(gameObject);
+            }
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag: " + tag + " doesn't exist");
+            return null;
+        }
+        var objectToSpawn = poolDictionary[tag].Dequeue();
+
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        var pooledObject = objectToSpawn.GetComponent<IPooledObject>();
+        if (pooledObject != null)
+        {
+            pooledObject.OnObjectSpawn();
+        }
+
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+}

075c9d9c2b72b38fa5a044d4546fab0d7daff7a1
Nils Lundell <nils.lundell@mardigras.se>
2022-06-13 08:04:05 +0200

    added random rotation to meteors

--- Assets/Scripts/Meteor.cs
+++ Assets/Scripts/Meteor.cs
@@ -6,11 +6,31 @@ public class Meteor : MonoBehaviour, IPooledObject
 {
     public float minFallSpeed;
     public float maxFallSpeed;
+    public float minRotationSpeed;
+    public float maxRotationSpeed;
     private float fallSpeed;
+    private float rotationSpeed;
+    public Rigidbody rigidbody;
+    
+
+    void Start()
+    {
+    }
 
     public void OnObjectSpawn()
     {
         fallSpeed = Random.Range(minFallSpeed, maxFallSpeed);
+        rotationSpeed = Random.Range(minRotationSpeed, maxRotationSpeed);
+
+        var xRot = Random.Range(-1, 1);
+        var yRot = Random.Range(-1, 1);
+        var zRot = Random.Range(-1, 1);
+        var rotationVector = new Vector3(xRot, yRot, zRot) * rotationSpeed;
+
+        if(rigidbody != null)
+        {
+            rigidbody.AddTorque(rotationVector);
+        }
     }
 
 
